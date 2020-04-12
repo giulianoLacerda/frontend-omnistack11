70c2f080fd9df489f21a08b0a9519be6b6f9b139
Giuliano <Giuliano>
2020-04-12 19:55:47 -0300

    Função de logout na tela de profile.

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,6 +10,8 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -32,11 +34,17 @@ export default function Profile() {
                     Authorization: ongId,
                 }
             });
-
+            // Update incidents list.
             setIncidents(incidents.filter(incident => incident.id !== id));
         } catch (error) {
             alert('Erro ao deletar caso, tente novamente.');
         }
+    }
+
+    function handleLogout() {
+        localStorage.clear(); // clear all local storage.
+
+        history.push('/');
     }
 
     return (
@@ -46,7 +54,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={() => handleLogout()}>
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
